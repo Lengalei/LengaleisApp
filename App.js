b3221aca9a3c1e7a74669bed283665a8c611e400
Lengalei <ruthlengalei@gmail.com>
2024-12-12 02:27:38 +0300

    updates

--- App.js
+++ App.js
@@ -4,9 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      {/* <button>Click Me</button> */}
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text>Hello World</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text>Worthy is your name Jesus</Text>
+        <Text>Worthy is your name Jesus</Text>
+        <Text>Worthy is your name Jesus</Text>
+      </View>
     </View>
   );
 }
@@ -17,5 +23,14 @@ const styles = StyleSheet.create({
     backgroundColor: "green",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "yellow",
+    padding: "20",
+    margin: "10",
+  },
+  body: {
+    backgroundColor: "pink",
+    padding: "20",
   },
 });
